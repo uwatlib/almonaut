d5e36b3e35a9a34dca924aee5c935b441d0dba10
University of Waterloo Library <114670103+uwatlib@users.noreply.github.com>
2022-09-30 09:36:35 -0400

    Update path for Sphinx autodoc.

--- docs/conf.py
+++ docs/conf.py
@@ -10,8 +10,8 @@ import os
 import sys
 from typing import Any, Dict
 
-src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../src"))
-sys.path.insert(0, src_path)
+root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, root_path)
 
 # -- Project information -----------------------------------------------------
 
